7d3fe0ffa9bd07b23526c40f5337b30889aa2d49
Giannis Zapantis <sdi1900059@di.uoa.gr>
2021-07-16 13:28:27 +0300

    Fix, 6.4

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -108,13 +108,13 @@ namespace StarChart.Controllers
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
-            var celestialObject = _context.CelestialObjects.Where(c => c.Id == id || c.OrbitedObjectId == id).ToList();
-            if (celestialObject == null)
+            var celestialObjects = _context.CelestialObjects.Where(c => c.Id == id || c.OrbitedObjectId == id).ToList();
+            if (!celestialObjects.Any())
             {
                 return NotFound();
             }
 
-            _context.CelestialObjects.RemoveRange(celestialObject);
+            _context.CelestialObjects.RemoveRange(celestialObjects);
             _context.SaveChanges();
 
             return NoContent();
